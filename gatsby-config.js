0656e94d4c0da6fb1a68ee749b61ab94df4bf865
nnnkit <iankit17@gmail.com>
2019-06-08 14:11:52 +0530

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  __experimentalThemes: [
+    {
+      resolve: "gatsby-theme-startgatsby",
+      options: {
+        name: "Nova Blog",
+        title: "Easiest way to create a blog!",
+        description: "Gatsby blog starter ...",
+        social: {
+          facebook: "altcampus",
+          twitter: "altcampus",
+          github: "altcampus",
+          email: "sample@example.com"
+        }
+      }
+    }
+  ]
+};
